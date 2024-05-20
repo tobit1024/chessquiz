45bdd19533e03f628e97ecc21615dfdcf476f84a
tobit1024 <61526826+tobit1024@users.noreply.github.com>
2024-05-20 21:29:45 +0900

    Add files via upload

---
+++ javascript/chess.js
@@ -0,0 +1,6 @@
+const Quiz = ["프렌치 디펜스", "카로칸 디펜스", "런던 시스템", "시실리안 디펜스", "킹즈 갬빗", "퀸즈 갬빗", "루이 로페즈", "스카치 갬빗", "이탈리안 게임"]
+
+const ChessQuiz = () => {
+    let answer = Math.floor(Math.random() * 10);
+
+}
